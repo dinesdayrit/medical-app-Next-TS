712b6627bf4b953e714a6ab626a603437df110a9
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-06-16 23:32:38 +0800

    feat: add signup form

--- app/(front)/(auth)/register/page.tsx
+++ app/(front)/(auth)/register/page.tsx
@@ -1,7 +1,20 @@
+import SignupForm from "@/components/auth/SignupForm";
+
+
+
+
 export default function page() {
   return (
-    <div>
-      <h2>Register</h2>
+    <div className="bg-blue-100 min-h-screen py-8 px-6 ">
+        <div className="grid md:grid-cols-2 grid-cols-1 w-full max-w-5xl mx-auto bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
+            <div className="hidden lg:block linear-bg">
+            {/* <Image src="/loginimage.jpg" alt="" height={1000} width={675} /> */}
+            </div>
+            
+            <div className="">
+                <SignupForm />
+            </div>
+        </div>
     </div>
   )
 }
--- app/(front)/layout.tsx
+++ app/(front)/layout.tsx
@@ -7,10 +7,10 @@ export default function Layout({children}:{children:ReactNode}) {
   return (
     <div className="bg-white">
       <Navbar />
-      <div className="bg-white mx-auto py-6 fixed top-20 w-full left-0 right-0 z-50 border-t border-gray-400/30">
+      {/* <div className="bg-white mx-auto py-6 fixed top-20 w-full left-0 right-0 z-50 border-t border-gray-400/30">
       <MegaMenu />
-      </div>
-      <div className="mt-[165px]">
+      </div> */}
+      <div className="mt-[80px]">
       {children}
       </div>
       
